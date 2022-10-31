e043f7e0a554c9e32adc7f79966d9d94ee244168
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2022-10-31 19:24:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
+// importing React from react library
+import React from "react";
+
+// importing css file 
+import "./css_files/app.css";
+
+
+// creating functional component
 function App() {
+  // options for dropdown list
+  const options = ["JavaScript", "HTML", "CSS", "REACTJS", "NODEJS", "NEXTJS"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h3>Select your option</h3>
+
+        <h4>You have selected</h4>
+
+        {/* creating drop down */}
+
+        <select>
+          {options.map(function (option) {
+            return <option className="option">{option}</option>;
+          })}
+          ;
+        </select>
+      </div>
     </div>
   );
+
 }
 
 export default App;
